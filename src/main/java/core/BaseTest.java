e372c4dbdff6590f45b69824271c02ceee6f4924
Shweta <shwetakast@gmail.com>
2025-05-01 17:26:03 +0530

    Api Project Commit

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import utils.ExtentReport;
+import utlis.ExtentReport;
 
 
 import java.io.IOException;
